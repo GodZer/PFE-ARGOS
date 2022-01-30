70b2880c3344a22285256bf30b068ea7b48a26ac
seb-jul <jul.sebastien@outlook.fr>
2022-01-30 17:40:15 +0100

    Ajout CreateTrainingJob RCF + Create Model RCF + modification des job names avec des paths supplémentaires

--- Projet/projet/innerStepFunction/innerFunction.py
+++ Projet/projet/innerStepFunction/innerFunction.py
@@ -36,12 +36,18 @@ class InnerFunction(Construct):
         valueCalculator = sf.Pass(self, "PathCalculator", 
             parameters={
                 "partition.$": "$.partition",
+                "Execution.Name.$": "$$.Execution.Name",
                 "object2vec_training_dataset_path.$": f"States.Format('s3://{datasetStorageBucket.bucket_name}/{{}}/training_O2V/', $.partition)",
                 "object2vec_inference_dataset_path.$": f"States.Format('s3://{datasetStorageBucket.bucket_name}/{{}}/ingestion_transform/', $.partition)",
                 "object2vec_inference_output_path.$": f"States.Format('s3://{datasetStorageBucket.bucket_name}/{{}}/ingestion_transform_output/', $.partition)",
                 "object2vec_model_path.$": f"States.Format('s3://{modelStorageBucket.bucket_name}/{{}}/model_O2V/', $.partition)",
                 "rcf_model_path.$": f"States.Format('s3://{modelStorageBucket.bucket_name}/{{}}/model_RCF/', $.partition)",
-                "rcf_training_dataset_path.$": f"States.Format('s3://{datasetStorageBucket.bucket_name}/{{}}/training_RCF/', $.partition)"
+                "rcf_training_dataset_path.$": f"States.Format('s3://{datasetStorageBucket.bucket_name}/{{}}/training_RCF/', $.partition)",
+                "Object2Vec-training-job.$": f"States.Format('Object2Vec-training-job-{{}}', $$.Execution.Name)",
+                "object2vec_model_name.$": f"States.Format('object2vec-model-name-{{}}', $$.Execution.Name)",
+                "Batch-transform-job.$": f"States.Format('Batch-transform-job-{{}}', $$.Execution.Name)",
+                "RCF-training-job.$": f"States.Format('RCF-training-job-{{}}', $$.Execution.Name)",
+                "RCF_model_name.$": f"States.Format('RCF-model-name-{{}}', $$.Execution.Name)"
             }
         )
 
@@ -72,7 +78,6 @@ class InnerFunction(Construct):
         )
 
         sagemaker_role.add_managed_policy(iam.ManagedPolicy.from_managed_policy_arn(self, "AmazonSageMakerFullAccess", "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"))
-
         sagemaker_role.add_managed_policy(iam.ManagedPolicy.from_managed_policy_arn(self, "S3FullAccess", "arn:aws:iam::aws:policy/AmazonS3FullAccess"))
 
         datasetStorageBucket.grant_read(sagemaker_role)
@@ -96,7 +101,7 @@ class InnerFunction(Construct):
         output_data_config=sft.OutputDataConfig(
             s3_output_location=sft.S3Location.from_json_expression("$.object2vec_model_path")
         ),
-        training_job_name=sf.JsonPath.string_at("$$.Execution.Name"),
+        training_job_name=sf.JsonPath.string_at("$.Object2Vec-training-job"),
         hyperparameters={
             "dropout": "0.2",
             "enc0_max_seq_len": "10",
@@ -126,14 +131,13 @@ class InnerFunction(Construct):
         )))
 
         createObject2VecModel= sft.SageMakerCreateModel(self, "CreateObject2VecModel",
-            model_name=sf.JsonPath.string_at("$$.Execution.Name"),
+            model_name=sf.JsonPath.string_at("$.object2vec_model_name"),
             primary_container= sft.ContainerDefinition(
                 image=sft.DockerImage.from_registry("749696950732.dkr.ecr.eu-west-3.amazonaws.com/object2vec:1"),
                 model_s3_location=sft.S3Location.from_json_expression("$.training_O2V.model_path")
             ),
             result_selector={
-                 "model_arn": sf.JsonPath.string_at("$.ModelArn"),
-                 "model_name": sf.JsonPath.string_at("$$.Execution.Name")
+                 "model_arn": sf.JsonPath.string_at("$.ModelArn")
             },
             result_path="$.object2vec_model"
         )
@@ -142,8 +146,8 @@ class InnerFunction(Construct):
         modelStorageBucket.grant_read(createObject2VecModel)
 
         batchTransformJob=sft.SageMakerCreateTransformJob(self, "BatchTransformJob",
-            transform_job_name=sf.JsonPath.string_at("$$.Execution.Name"),
-            model_name=sf.JsonPath.string_at("$.object2vec_model.model_name"),
+            transform_job_name=sf.JsonPath.string_at("$.Batch-transform-job"),
+            model_name=sf.JsonPath.string_at("$.object2vec_model_name"),
             transform_input=sft.TransformInput(
                 transform_data_source=sft.TransformDataSource(
                     s3_data_source=sft.TransformS3DataSource(
@@ -155,7 +159,7 @@ class InnerFunction(Construct):
             transform_output=sft.TransformOutput(
                 s3_output_path=sf.JsonPath.string_at("$.object2vec_inference_output_path")
             ),
-            transform_resources=sft.TransformResources(instance_count=1, instance_type=ec2.InstanceType("m5.4xlarge")),
+            transform_resources=sft.TransformResources(instance_count=2, instance_type=ec2.InstanceType("m5.4xlarge")),
             integration_pattern=sf.IntegrationPattern.RUN_JOB,
             result_path=sf.JsonPath.DISCARD
         )
@@ -183,10 +187,61 @@ class InnerFunction(Construct):
             result_path=sf.JsonPath.DISCARD
         )
 
-        
+        trainingRCF = sft.SageMakerCreateTrainingJob(self, "CreateRCFTrainingJob",
+        role=sagemaker_role,
+        algorithm_specification=sft.AlgorithmSpecification(
+            training_image=sft.DockerImage.from_registry("749696950732.dkr.ecr.eu-west-3.amazonaws.com/randomcutforest:1"),
+            training_input_mode=sft.InputMode.FILE
+        ),
+        input_data_config=[sft.Channel(
+            channel_name="train",
+            data_source=sft.DataSource(
+                s3_data_source=sft.S3DataSource(
+                    s3_location=sft.S3Location.from_json_expression("$.rcf_training_dataset_path")
+                )
+            ),
+            content_type="text/csv;label_size=0"
+        )
+        ],
+        output_data_config=sft.OutputDataConfig(
+            s3_output_location=sft.S3Location.from_json_expression("$.rcf_model_path")
+        ),
+        training_job_name=sf.JsonPath.string_at("$.RCF-training-job"),
+        hyperparameters={
+            "feature_dim": "10",
+            "num_samples_per_tree": "256",
+            "num_trees": "100" 
+        },
+        integration_pattern=sf.IntegrationPattern.RUN_JOB,
+        resource_config=sft.ResourceConfig(instance_count=1, instance_type=ec2.InstanceType("m5.4xlarge"), volume_size=Size.gibibytes(500)),
+        result_selector={
+            "model_path": sf.JsonPath.string_at("$.ModelArtifacts.S3ModelArtifacts")
+        },
+        result_path="$.training_RCF"
+        )
+
+        trainingRCF.role.attach_inline_policy(iam.Policy(self, "GrantPassRoleRCF", document=iam.PolicyDocument(
+            statements=[iam.PolicyStatement(actions=["iam:PassRole"], resources=[sagemaker_role.role_arn])]
+        )))
+
+        createRCFModel= sft.SageMakerCreateModel(self, "CreateRCFModel",
+            model_name=sf.JsonPath.string_at("$.RCF_model_name"),
+            primary_container= sft.ContainerDefinition(
+                image=sft.DockerImage.from_registry("749696950732.dkr.ecr.eu-west-3.amazonaws.com/randomcutforest:1"),
+                model_s3_location=sft.S3Location.from_json_expression("$.training_RCF.model_path")
+            ),
+            result_selector={
+                 "model_arn": sf.JsonPath.string_at("$.ModelArn")
+            },
+            result_path="$.RCF_model"
+        )
+
+        datasetStorageBucket.grant_read_write(createRCFModel)
+        modelStorageBucket.grant_read(createRCFModel)
+
         #Create Chain        
 
-        template=valueCalculator.next(glueJobObject2Vec).next(trainingObject2Vec).next(createObject2VecModel).next(batchTransformJob).next(glueJobRCF)
+        template=valueCalculator.next(glueJobObject2Vec).next(trainingObject2Vec).next(createObject2VecModel).next(batchTransformJob).next(glueJobRCF).next(trainingRCF).next(createRCFModel)
 
         #Create state machine
 
